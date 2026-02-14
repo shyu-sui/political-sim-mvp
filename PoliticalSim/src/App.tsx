331234455f6bad1c6747be2cdafa2a1b2c0a79ec
AzureAD\ZengoIori <izengo@brain-j.co.jp>
2026-02-14 18:18:26 +0900

    不要importをコメントアウト

--- PoliticalSim/src/App.tsx
+++ PoliticalSim/src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-import Timeline from './components/Timeline';
+import Timeline from './components/TimeLine.tsx';
 
 
 function App() {

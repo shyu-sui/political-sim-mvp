bc014192b54edecd575ac4c8b431ab0d7bd6e7f5
AzureAD\ZengoIori <izengo@brain-j.co.jp>
2026-02-14 19:18:14 +0900

    fix(ui): 文字色の明示・selectの可視

--- PoliticalSim/src/App.tsx
+++ PoliticalSim/src/App.tsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Timeline from './components/TimeLine.tsx';
+import Timeline from './components/TimeLine';
 
 
 function App() {
